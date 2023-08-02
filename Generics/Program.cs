17e69e939e0f3e64dc9f29636eb279c414530555
R.K Nanda <rk@RK-s-MacBook-Air.local>
2023-08-02 16:46:01 +0530

    [RK Nanda] Adding static method to give largest int of 3 integers

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -3,7 +3,16 @@ class Program
 {
     static void Main(string[] args)
     {
+        int x, y, z,res;
+
         Console.WriteLine("Welcome to Generics!");
+        Console.WriteLine("Enter x");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter y");
+        y = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter z");
+        z = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Maximum integer is {Utility.LargestInt(x, y, z)}");
         Console.ReadLine();
     }
 }
---
+++ Generics/Utility.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Generics
+{
+	public class Utility
+	{
+		public static int LargestInt(int x, int y, int z)
+		{
+            
+                if (x >= y)
+                {
+                    if (x >= z)
+                    {
+                        return x;
+                    }
+                    else
+                    {
+
+                        return z;
+
+                    }
+
+                }
+
+                else
+                {
+                    if (y >= z)
+                    {
+                        return y;
+                    }
+                    else
+                    {
+                        return z;
+                    }
+                }
+        }
+	}
+}
+
