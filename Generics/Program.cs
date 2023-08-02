0d4572ad1d334e72ea8bd5d911cbbb2879ec7676
R.K Nanda <rk@RK-s-MacBook-Air.local>
2023-08-02 17:49:30 +0530

    [RK Nanda] Adding method to calculate max in array

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -3,72 +3,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        
-        int option;
-        bool flag = true;
-
-        while(flag)
+        int n, y, z;
+        Console.WriteLine("Enter size of array");
+        n = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[n];
+        for(int i =0; i<n; i++)
         {
-            Console.WriteLine("Menu");
-            Console.WriteLine("1.Max Of 3 Integers");
-            Console.WriteLine("2.Max Of 3 Float");
-            Console.WriteLine("3.Max Of 3 Strings");
-            Console.WriteLine("0.Exit");
-            option = Convert.ToInt32(Console.ReadLine());
-
-            switch(option)
-            {
-                case 0:
-                    {
-                        flag = false;
-                        break;
-                    }
-
-                case 1:
-                    {
-                        int x, y, z;
-                        Console.WriteLine("Enter x");
-                        x = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Enter y");
-                        y = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Enter z");
-                        z = Convert.ToInt32(Console.ReadLine());
-                        GenericUtility<int> obj = new GenericUtility<int>(x, y, z);
-                        Console.WriteLine($"The maximum value is: {obj.TestMaximum()}");
-                        break;
-
-                    }
-                case 2:
-                    {
-                        float x, y, z;
-                        Console.WriteLine("Enter x");
-                        x = Convert.ToSingle(Console.ReadLine());
-                        Console.WriteLine("Enter y");
-                        y = Convert.ToSingle(Console.ReadLine());
-                        Console.WriteLine("Enter z");
-                        z = Convert.ToSingle(Console.ReadLine());
-                        GenericUtility<float> obj = new GenericUtility<float>(x, y, z);
-                        Console.WriteLine($"The maximum value is: {obj.TestMaximum()}");
-                        break;
-
-                    }
-                case 3:
-                    {
-                        string x, y, z;
-                        Console.WriteLine("Enter x");
-                        x = Console.ReadLine();
-                        Console.WriteLine("Enter y");
-                        y = Console.ReadLine();
-                        Console.WriteLine("Enter z");
-                        z = Console.ReadLine();
-                        GenericUtility<string> obj = new GenericUtility<string>(x, y, z);
-                        Console.WriteLine($"The maximum value is: {obj.TestMaximum()}");
-                        break;
-
-                    }
-            }
+            arr[i] =Convert.ToInt32(Console.ReadLine());
         }
 
+        GenericUtility<int> obj = new GenericUtility<int>();
+        Console.WriteLine($"The maximum value is: {GenericUtility<int>.GreatestInArray(arr)}");
+        Console.ReadLine();
+    
     }
 
 
--- Generics/Utility.cs
+++ Generics/Utility.cs
@@ -3,32 +3,13 @@ namespace Generics
 {
     public class GenericUtility<T> where T : IComparable<T>
     {
-        private T a, b, c;
-
-        public GenericUtility(T val1, T val2, T val3)
+        public static T GreatestInArray(T[] arr)
         {
-            a = val1;
-            b = val2;
-            c = val3;
+            Array.Sort(arr);
+            int sizeOfArray = arr.Length;
+            T maxElement = arr[sizeOfArray - 1];
+            return maxElement;
 
-        }
-        public T TestMaximum()
-        {
-            return GetMaximum(a, b, c);
-        }
-
-        public static T GetMaximum(T a, T b, T c) 
-        {
-            T max = a;
-            if (b.CompareTo(max) > 0)
-            {
-                max = b;
-            }
-            if (c.CompareTo(max) > 0)
-            {
-                max = c;
-            }
-            return max;
         }
 
         
